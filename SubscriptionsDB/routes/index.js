3b90caeb26ddd1903bdc0c72af7ce953b8a80a56
yanagiragi <qaz90473@gmail.com>
2020-01-05 16:11:43 +0800

    setup basic interface

--- SubscriptionsDB/routes/index.js
+++ SubscriptionsDB/routes/index.js
@@ -1,9 +1,33 @@
-var express = require('express');
-var router = express.Router();
+const express = require('express');
+const router = express.Router();
 
-/* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.send('Yello. This is SubscriptionDB entry point.')
+});
+
+// api for notification
+router.get('/containerType', function(req, res, next) {
+  res.send('Yello.')
+});
+
+router.get('/notice/:containerId/:entryId', function(req, res, next) {
+  const containerId = req.params.containerId
+  const entryId = req.params.entryId
+  console.log(containerId)
+  console.log(entryId)
+  res.send('respond with a resource' + containerId + ' ' + entryId);
+});
+
+router.get('/noticeAll/:containerId', function(req, res, next) {
+  const containerId = req.params.containerId
+  console.log(containerId)
+  console.log(entryId)
+  res.send('respond with a resource' + containerId + ' ' + entryId);
+});
+
+router.post('/addEntry', function(req, res, next) {
+  console.log(req.body)
+  res.send('respond with a resource' + JSON.stringify(req.body));
 });
 
 module.exports = router;
