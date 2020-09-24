0f0ee28f3b4bda98776121012944496b9d4f581e
yanagiragi <qaz90473@gmail.com>
2020-09-24 01:28:12 +0000

    Add Cache for speedup

--- core/db.js
+++ core/db.js
@@ -6,22 +6,62 @@ class SubscriptionsDB {
 		this.client = new Client(setting.clientSetting)
 		this.table = setting.table,			
 		this.client.connect()
+		this.cache = []
+		this.unNoticedCache = []
+		this.nicknameCache = []
+
+		this.InitCache(this)
+		setInterval( () => this.InitCache(this), 1000 * 30);
+	}
+
+	async InitCache(db)
+	{
+		let query = {
+			text: `SELECT * FROM ${this.table};`,
+			values: [],
+		}
+		let result = await db.Query(query)
+		db.cache = result.rows
+		
+		query = {
+			text: `SELECT * FROM ${this.table} WHERE isnoticed = false;`,
+			values: [],
+		}
+		result = await db.Query(query)
+		db.unNoticedCache = result.rows
+		
+		query = {
+			text: `SELECT DISTINCT type FROM ${this.table};`,
+			values: [],
+		}
+		result = await db.Query(query)
+		db.nicknameCache = result.rows
 	}
 
 	Query(option)
 	{
 		return new Promise((resolve, reject) =>
-		{
+		{	
+			Logger.log({
+				console: 'true',
+				level: 'info',
+				message: `Query: ${JSON.stringify(option)}`
+			});
 			this.client.query(option, (err, res) => resolve(res))
+			Logger.log({
+				console: 'true',
+				level: 'info',
+				message: `Query: ${JSON.stringify(option)} Done.`
+			});
 		});
 	}
 
 	/*
-    *	Params:
-    *		containerId: container.container 中的 Index
-    *		listId: container.container[index].list 的 Index
-    *
-    */
+    	*	Params:
+	*		containerId: container.container 中的 Index
+	*		listId: container.container[index].list 的 Index
+	*
+	*/
 	async NoticeEntry (id) {
 		const result = await this.Query({
 			text: `UPDATE ${this.table} SET ISNOTICED = true where id = $1;`,
@@ -30,16 +70,23 @@ class SubscriptionsDB {
 		
 		const isEntryExists = result.rowCount && result.rowCount > 0;
 		if (isEntryExists) {
-
-			const result = await this.Query({
-				text: `SELECT * FROM ${this.table} where id = $1;`,
-				values: [ id ],
-			})
+			const isEntryExistsInCache = this.cache.filter(x => x.id == id).length > 0
+			let title = ''
+			if (!isEntryExistsInCache) {
+				const result = await this.Query({
+					text: `SELECT * FROM ${this.table} where id = $1;`,
+					values: [ id ],
+				})
+				title = result.rows[0].title
+			}
+			else {
+				title = this.cache.filter(x => x.id == id)[0].title
+			}
 
 			Logger.log({
 				console: 'true',
 				level: 'info',
-				message: `Read ContainerId <${id}>: ${result.rows[0].title}`
+				message: `Read ContainerId <${id}>: ${title}`
 			});
 		} else {
 			Logger.log({
@@ -59,11 +106,31 @@ class SubscriptionsDB {
 			return 'Invalid Entry';
 		}
 
-		const result = await this.Query({
-			text: `SELECT * FROM ${this.table} WHERE nickname = $1 AND title = $2 AND href = $3 AND img = $4;`,
-			values: [ nickname, data.title, data.href, data.img ],
-		})
-		const existed = result.rowCount && result.rowCount > 0;
+		let existed = this.cache.filter(x => 
+			x.title == data.title &&
+			x.nickname == nickname &&
+			x.href == data.href &&
+			x.img == data.img
+			).length > 0;
+		if (existed == false) {
+			const result = await this.Query({
+				text: `SELECT * FROM ${this.table} WHERE nickname = $1 AND title = $2 AND href = $3 AND img = $4;`,
+				values: [ nickname, data.title, data.href, data.img ],
+			})
+			existed = result.rowCount && result.rowCount > 0;
+		}
+		/*else { 
+			Logger.log({
+				level: 'info',
+				message: `Detect existed Entry in Cache: ${this.cache.filter(x =>
+		                        x.title == data.title &&
+		                        x.nickname == nickname &&
+		                        x.href == data.href &&
+		                        x.img == data.img
+		                        )[0].title}, Skip.`
+			});
+		}*/
+
 		const isValid = data && data.title && data.href && data.img;
 
 		if (isValid && !existed) {			
@@ -100,7 +167,7 @@ class SubscriptionsDB {
 	async ConvertToOldFormat(result)
 	{
 		const types = await this.GetContainerTypes()		
-		const nicknames = [...new Set(result.rows.map(x => x.nickname))]
+		const nicknames = [...new Set(result.map(x => x.nickname))]
 
 		const parsed = 
 		{
@@ -108,7 +175,7 @@ class SubscriptionsDB {
 			container: []
 		}
 		
-		for(const row of result.rows)
+		for(const row of result)
 		{
 			var typeIdx = parsed.types.indexOf(row.type);
 			var containerIdx = parsed.container.findIndex(x => x.typeId === typeIdx && x.nickname === row.nickname);
@@ -130,30 +197,18 @@ class SubscriptionsDB {
 	}
 
 	async GetContainerTypes () {
-		const query = {
-			text: `SELECT DISTINCT type FROM ${this.table};`,
-			values: [],
-		}
-		const result = await this.Query(query)
-		return result.rows.map(x => x.type);
+		Logger.log({ level: 'info', message: 'Get Nickname, Return cache' });
+		return this.nicknameCache;
 	}
 
 	async GetContainer() {
-		const query = {
-			text: `SELECT * FROM ${this.table};`,
-			values: [],
-		}
-		const result = await this.Query(query)
-		return this.ConvertToOldFormat(result)
+		Logger.log({ level: 'info', message: 'Get Container, Return cache' });
+		return this.ConvertToOldFormat(this.cache)
 	}
 
 	async GetUnNoticedContainers() {
-		const query = {
-			text: `SELECT * FROM ${this.table} WHERE isNoticed = false;`,
-			values: [],
-		}
-		const result = await this.Query(query)
-		return this.ConvertToOldFormat(result)
+		Logger.log({ level: 'info', message: 'Get unNoticed Container, Return cache' });
+		return this.ConvertToOldFormat(this.unNoticedCache)
 	}
 }
 
