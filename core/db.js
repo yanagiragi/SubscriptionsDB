28ab55e1c996673d200810802768d78f5e879003
yanagiragi <qaz90473@gmail.com>
2020-09-28 19:13:35 +0000

    Add task queue to solve request sequentially

--- core/db.js
+++ core/db.js
@@ -9,51 +9,96 @@ class SubscriptionsDB {
 		this.cache = []
 		this.unNoticedCache = []
 		this.nicknameCache = []
+		this.isDirty = true
+
+		this.queue = []
 
 		this.InitCache(this)
-		setInterval( () => this.InitCache(this), 1000 * 30);
+		
+		setInterval(this.InitCache.bind(this), 1000 * 30);
+
+		setInterval(this.DealQuery.bind(this), 1000 * 0.5);
 	}
 
-	async InitCache(db)
+	async InitCache()
 	{
+		this.isDirty = true
+
+		Logger.log({ level: 'info', message: 'Read DB' })
+
+		const func = (option) =>
+		{
+			return new Promise((resolve, reject) =>
+			{	
+				this.client.query(option, (err, res) => resolve(res))
+			});
+		}
+
 		let query = {
 			text: `SELECT * FROM ${this.table};`,
 			values: [],
 		}
-		let result = await db.Query(query)
-		db.cache = result.rows
+		let result = await func(query);
+		this.cache = result.rows
 		
 		query = {
 			text: `SELECT * FROM ${this.table} WHERE isnoticed = false;`,
 			values: [],
 		}
-		result = await db.Query(query)
-		db.unNoticedCache = result.rows
+		result = await func(query);
+		this.unNoticedCache = result.rows
 		
 		query = {
 			text: `SELECT DISTINCT type FROM ${this.table};`,
 			values: [],
 		}
-		result = await db.Query(query)
-		db.nicknameCache = result.rows
+		result = await func(query);
+		this.nicknameCache = result.rows.map(x => x.type)
+
+		Logger.log({ level: 'info', message: 'Read DB Done' })
+		
+		this.isDirty = false
 	}
 
-	Query(option)
+	DealQuery()
 	{
-		return new Promise((resolve, reject) =>
-		{	
-			Logger.log({
-				console: 'true',
-				level: 'info',
-				message: `Query: ${JSON.stringify(option)}`
-			});
-			this.client.query(option, (err, res) => resolve(res))
-			Logger.log({
-				console: 'true',
-				level: 'info',
-				message: `Query: ${JSON.stringify(option)} Done.`
-			});
-		});
+
+		if (this.isDirty) return; 
+		if (this.queue.length == 0) return;
+
+		const task = this.queue.pop();
+		this.client.query(task, (err, res) => {
+			if (err) {
+				this.queue.push(task)
+			}
+			else {
+				if (task.text.indexOf('INSERT') == 0)
+				{
+					Logger.log({
+						level: 'info',
+						message: `[${this.queue.length}] Add New Entry, title = <${task.values[0]}>`
+					});
+				}
+				else if (task.text.indexOf('UPDATE') == 0)
+				{
+					Logger.log({
+						level: 'info',
+						message: `[${this.queue.length}] Read Entry, title = <${task.values}>`
+					});
+				}
+				else {
+					Logger.log({
+						level: 'info',
+						message: `Query: [${this.queue.length}]: ${JSON.stringify(task)}`
+					});
+				}
+			}
+		})
+	}
+
+	Query(option, prefix = "BEGIN TRANSACTION ISOLATION LEVEL SERIALIZABLE;", postfix = "COMMIT;")
+	{
+		this.queue.push(option)
 	}
 
 	/*
@@ -112,13 +157,13 @@ class SubscriptionsDB {
 			x.href == data.href &&
 			x.img == data.img
 			).length > 0;
-		if (existed == false) {
+		/*if (existed == false) {
 			const result = await this.Query({
 				text: `SELECT * FROM ${this.table} WHERE nickname = $1 AND title = $2 AND href = $3 AND img = $4;`,
 				values: [ nickname, data.title, data.href, data.img ],
 			})
 			existed = result.rowCount && result.rowCount > 0;
-		}
+		}*/
 		/*else { 
 			Logger.log({
 				level: 'info',
@@ -134,14 +179,14 @@ class SubscriptionsDB {
 		const isValid = data && data.title && data.href && data.img;
 
 		if (isValid && !existed) {			
-			await this.Query({
+			/*await this.Query({
 				text: `INSERT INTO ${this.table} (title, href, img, isNoticed, type, nickname) VALUES ($1, $2, $3, $4, $5, $6);`,
 				values: [ data.title, data.href, data.img, false, containerType, nickname ],
+			})*/
+			await this.Query({
+				text: `INSERT INTO ${this.table} (title, href, img, isNoticed, type, nickname) SELECT $1, $2, $3, $4, $5, $6 WHERE NOT EXISTS ( SELECT 1 FROM ${this.table} WHERE title = $7 AND href = $8 AND img = $9 AND type = $10 AND nickname = $11 );`,
+				values: [ data.title, data.href, data.img, false, containerType, nickname, data.title, data.href, data.img, containerType, nickname ],
 			})
-			Logger.log({
-				level: 'info',
-				message: `Add New Entry, title = <${data.title}>`
-			});
 		} else {
 			if (existed) {
 				Logger.log({
@@ -166,7 +211,7 @@ class SubscriptionsDB {
 
 	async ConvertToOldFormat(result)
 	{
-		const types = await this.GetContainerTypes()		
+		const types = await this.GetContainerTypes()
 		const nicknames = [...new Set(result.map(x => x.nickname))]
 
 		const parsed = 
@@ -208,7 +253,8 @@ class SubscriptionsDB {
 
 	async GetUnNoticedContainers() {
 		Logger.log({ level: 'info', message: 'Get unNoticed Container, Return cache' });
-		return this.ConvertToOldFormat(this.unNoticedCache)
+		const result = await this.client.query( { text: `SELECT * FROM ${this.table} WHERE isnoticed = false;` } )
+		return this.ConvertToOldFormat(result.rows)
 	}
 }
 
